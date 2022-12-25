468cb653f0a0b8ca640843c9135ee54332007144
Abhishek <myselfabhi56@gmail.com>
2022-12-25 10:39:04 +0530

    finally updated porfolio

---
+++ src/Components/Github.jsx
@@ -0,0 +1,47 @@
+
+import React,{useEffect} from 'react'
+import styled from 'styled-components';
+import GithubCalendar from 'react-github-calendar'
+import stats from "../Resources/Images/stats.png"
+import Aos from "aos";
+import "aos/dist/aos.css";
+const Github = () => {
+
+    useEffect(() => {
+        Aos.init({duration:2000})
+       })
+
+    return (
+        <MainContainer style={{ marginTop: "-20px" }} >
+            <h1 style={{ color: "#343a40", textTransform: "uppercase" }}>GITHUB STATISTICS</h1>
+            <Container data-aos='fade-right'>
+                <GithubCalendar username='Abhi5647'year={new Date().getFullYear()} blockSize={15} blockMargin={5} fontSize={16} />
+            </Container>
+            {/* <img src={stats} alt="github stats" data-aos='fade-right' /> */}
+        </MainContainer>
+    )
+}
+
+const Container = styled.div`
+    margin:auto;
+    width:80%;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    margin-bottom:50px;
+    flex-direction:column;
+    // border: 4px solid #23465a;
+    border-radius:10px 10px 10px 10px;
+    padding:30px 0px 30px 0px;
+    
+`
+
+
+
+const MainContainer = styled.div`
+    @media (max-width: 50em){
+        display:none;
+    }
+`
+
+export default Github
---
+++ src/Components/Projects.jsx
@@ -0,0 +1,415 @@
+import React from 'react';
+import styled from 'styled-components';
+import dnalp from '../Resources/Images/Project/dnalp.png';
+import gearbestlp from '../Resources/Images/Project/gearbestlp.PNG';
+import senselp from '../Resources/Images/Project/senselp.png';
+import sugarlp from "../Resources/Images/Project/Sugarlp.PNG"
+import GitHubIcon from '@mui/icons-material/GitHub';
+import StreamIcon from '@mui/icons-material/Stream';
+
+
+const Projects = () => {
+
+    const projects = [
+        {
+            id: 1,
+            title: 'Ssense',
+            img: senselp,
+            desc: 'SSENSE is a multi-brand retailer based in Montreal, Canada specialising in the sale of designer fashion and high end streetwear.',
+            worked: 'This is an individual project. It was completed in 5 days',
+            keyPoint2: '',
+            keyPoint1: '',
+            githubLink: 'https://github.com/shekhar74/naive-plot-5053',
+            deployedLink: 'https://ssense-eight.vercel.app/',
+            tags: ['HTML', 'CSS', 'Javascript']
+        },
+        {
+        id: 2,
+        title: 'CosmoStore',
+        img: sugarlp,
+        desc: "CosmoStore is one of India's fastest-growing premium cosmetic companies. The brand's bestselling goods are in the Lips, Eyes, Face, Nails & Skin categories",
+        worked: 'A collaborative project, built in 5 days by a team of 4 developers.',
+        keyPoint2: '',
+        keyPoint1: '',
+        githubLink: 'https://github.com/Deep579007/-radioactive-care-9325',
+        deployedLink: 'https://cosmo-store-pi.vercel.app/',
+        tags: ['HTML', 'CSS', 'Javascript','React','Chakra-ui']
+        },
+        {
+            id: 3,
+            title: 'Dna-India',
+            img: dnalp,
+            desc: 'DNA India is an India-based online news website that covers all the latest and breaking news on Politics, Business, Sports, Bollywood, and Technology news.',
+            worked: 'A collaborative project, built in 5 days by a team of 5 developers.',
+            keyPoint2: 'This is our first project at Masai School.',
+            keyPoint1: 'Group project',
+            githubLink: 'https://github.com/sameerbhavar/dnaindia.com',
+            deployedLink: 'https://marvelous-dolphin-b7b476.netlify.app/',
+            tags: ['HTML', 'CSS', 'Javascript']
+        },
+        {
+            id: 4,
+            title: 'Gearbest',
+            img: gearbestlp,
+            desc: 'A lifestyle store is a retail store selling a wide variety of product categories under a single brand. It is designed to associate a brand with one or another aspirational lifestyle. Lifestyle stores may include clothing, housewares,etc.',
+            worked: 'This is an individual project completed in 5 days.',
+            keyPoint1: 'local json server used',
+            keyPoint2: '',
+            githubLink: 'https://github.com/shekhar74/meek-calculator-9744',
+            deployedLink: 'https://gearbestdotcom.vercel.app/',
+            tags: ['HTML', 'CSS','Javascript', 'Reactjs', 'Chakra-Ui']
+        }
+    ]
+
+    return (
+        <Container id='projects'>
+            <h1>Projects</h1>
+            <div className='projectsContainer' >
+                {projects.map((ele, index) => (
+                    <div key={index} className="box">
+                        <div className='imgBox'>
+                            <img src={ele.img} alt={ele.title} width='120%' height='100%' />
+                        </div>
+                        <div className='ContentBox'>
+                            <h1>{ele.title}</h1>
+                            <p>{ele.desc}</p>
+                            <p style={{ fontWeight: 'bold' }}>{ele.worked}</p>
+                            <div className='tagsBox'>
+                                {ele.tags.map((tag, index) => (
+                                    <span key={index}>{tag}</span>
+                                ))}
+                            </div>
+                            <div className='linkSection'>
+                                <a href={ele.githubLink} rel="noreferrer" target="_blank"><button><GitHubIcon />Github</button></a>
+                                <a href={ele.deployedLink} rel="noreferrer" target="_blank"><button><StreamIcon />Live</button></a>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </Container >
+    )
+}
+
+
+const Container = styled.div`
+    width:100%;
+    margin:auto;
+    display:flex;
+    flex-direction:column;
+    gap:20px;
+    
+    h1{
+        color:#343a40;
+        text-transform:uppercase;
+    }
+
+    .projectsContainer{
+        width:80%;
+        margin:auto;
+        display:flex;
+        align-items:center;
+        flex-direction:column;
+    }
+
+    .box{
+        position:relative;
+        width:100%;
+        height:450px;
+        display:flex;
+        gap:10px;
+        margin-bottom:80px;
+        border: 10px solid lightgrey;
+        border-radius: 20px;
+        overflow:hidden;
+    }
+
+    .imgBox{
+        width:50%;
+        overflow:hidden;
+    }
+
+    
+    .imgBox>img{
+        display: block;
+        transition: transform .4s; 
+        width:100%;
+    }
+
+    // .imgBox>img:hover{
+    //     position:absolute;
+    //     width:80%;
+    //     transform: scale(1.3);
+    //     transform-origin: 0% 0%;
+    // }
+
+    
+    .ContentBox{
+        width:50%;
+        text-align:left;
+        padding:0px 20px 20px 20px;
+        
+    }
+
+    .ContentBox>h1{
+        color:#222e50;
+    }
+
+    .ContentBox>p{
+        width:90%;
+        font-size:large;
+        margin-top:0px;
+    }
+
+    .tagsBox{
+        width:80%;
+        display:flex;
+        align-items:center;
+        flex-wrap: wrap;
+        line-height:20px;
+        gap:5px;
+        margin-bottom:20px;
+    }
+
+    .tagsBox>span{
+        color:white;
+        background-color:#222e50;
+        padding: 2px 15px 2px 15px;
+        border-radius:10px;
+        font-size:13px;
+        font-weight:bold;
+    }
+
+
+    .linkSection{
+        display:flex;
+        gap:20px;
+    }
+
+    .linkSection>a{
+        text-decoration:none;
+    }
+    .linkSection>a:first-child>button{
+        background-color:black;
+        color: White;
+        padding:8px 20px 8px 20px;
+        border-radius:8px;
+        font-weight:bold;
+        display:flex;
+        align-items:center;
+        justify-content:center;
+        gap:10px;
+        border:none;
+        cursor:pointer;
+    }
+
+    button{
+        cursor:pointer;
+    }
+
+    button:hover{
+        -webkit-transform: translateY(-2px);
+        transform: translateY(-2px);
+        -webkit-transition: all 0.5s;
+        transition: all 0.5s;
+    }
+    
+
+    .linkSection>a:last-child>button{
+        background-color:black;
+        color: White;
+        padding:8px 20px 8px 20px;
+        border-radius:8px;
+        border:none;
+        font-weight:bold;
+        display:flex;
+        align-items:center;
+        justify-content:center;
+        gap:10px;
+    }
+
+    @media only screen and (min-width: 768px) and (max-width:1120px){
+        .box{
+            flex-direction:column;
+            align-items:center;
+            height:100%;
+        }
+ 
+        .projectsContainer{
+            width:80%;
+            margin:auto;
+            
+        }
+        
+        .imgBox{
+            width:100%;
+            height:400px;
+        }
+
+        .imgBox>img{
+            width:100%;
+        }
+
+        .ContentBox{
+            width:80%;
+            display:flex;
+            align-items:center;
+            justify-content:center;
+            flex-direction:column;
+        }
+
+        .ContentBox>h1{
+            font-size:x-large;
+        }
+
+        .ContentBox>p{
+            font-size:15px;
+        }
+
+        .tagsBox{
+            width:100%;
+            justify-content:center;
+        }
+    }
+
+    @media only screen and (min-width: 481px) and (max-width:768px){
+        .box{
+           flex-direction:column;
+           align-items:center;
+           height:100%;
+        }
+
+        .projectsContainer{
+            width:80%;
+            margin:auto;
+            
+        }
+        
+        .imgBox{
+            width:100%;
+            height:280px;
+        }
+
+        .imgBox>img{
+            width:100%;
+        }
+
+        .ContentBox{
+            width:80%;
+            display:flex;
+            align-items:center;
+            justify-content:center;
+            flex-direction:column;
+            text-align:center;
+        }
+
+        .ContentBox>h1{
+            font-size:x-large;
+        }
+
+        .ContentBox>p{
+            font-size:15px;
+        }
+
+        .tagsBox{
+            width:100%;
+            justify-content:center;
+        }
+    }
+
+
+    @media only screen and (min-width:320px) and (max-width:480px){
+        .box{
+            flex-direction:column;
+            align-items:center;
+            height:100%;
+        }
+
+        .projectsContainer{
+            width:90%;
+            margin:auto;
+            
+        }
+
+        .imgBox{
+            width:100%;
+            height:200px;
+        }
+
+        .imgBox>img{
+            width:100%;
+        }
+
+        .ContentBox{
+            width:80%;
+            display:flex;
+            align-items:center;
+            justify-content:center;
+            flex-direction:column;
+            text-align:center;
+        }
+
+        .ContentBox>h1{
+            font-size:large;
+        }
+
+        .ContentBox>p{
+            font-size:13px;
+            
+        }
+
+
+        .tagsBox{
+            width:100%;
+            justify-content:center;
+        }
+    }
+
+    @media only screen and (max-width: 319px){
+        .box{
+            flex-direction:column;
+            align-items:center;
+            height:100%;
+        }
+
+        .projectsContainer{
+            width:90%;
+            margin:auto;
+            
+        }
+
+        .imgBox{
+            width:100%;
+            height:150px;
+        }
+
+        .imgBox>img{
+            width:100%;
+            
+        }
+
+        .ContentBox{
+            width:80%;
+            display:flex;
+            align-items:center;
+            justify-content:center;
+            flex-direction:column;
+            text-align:center;
+        }
+
+        .ContentBox>h1{
+            font-size:large;
+        }
+
+        .ContentBox>p{
+            font-size:13px;
+        }
+
+
+        .tagsBox{
+            width:100%;
+            justify-content:center;
+        }
+    }
+`
+export default Projects
